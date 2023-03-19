3f53a2d81df27d63eeccda6f48f77bc26dc3af8b
Ferhad <mislim.sheridancollege.ca>
2023-03-19 01:29:04 -0400

    added Requester class

--- src/main/java/ca/sheridancollege/blockheads/bridgingblock/beans/Requester.java
+++ src/main/java/ca/sheridancollege/blockheads/bridgingblock/beans/Requester.java
@@ -1,2 +1,25 @@
-package ca.sheridancollege.blockheads.bridgingblock.beans;public class Requestor {
+package ca.sheridancollege.blockheads.bridgingblock.beans;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Builder
+public class Requester {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
 }
