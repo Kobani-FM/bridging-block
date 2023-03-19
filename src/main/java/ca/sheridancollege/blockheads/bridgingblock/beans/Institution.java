7391bb584541e03be1496afd6e6dce9a59abde0a
Ferhad <mislim.sheridancollege.ca>
2023-03-19 01:28:14 -0400

    added Institution class

--- src/main/java/ca/sheridancollege/blockheads/bridgingblock/beans/Institution.java
+++ src/main/java/ca/sheridancollege/blockheads/bridgingblock/beans/Institution.java
@@ -1,2 +1,25 @@
-package ca.sheridancollege.blockheads.bridgingblock.beans;public class Institution {
+package ca.sheridancollege.blockheads.bridgingblock.beans;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Builder
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private InstitutionStatus status;
+
+
 }
