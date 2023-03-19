3e361570d5c2148be659eead93acb02056b9afd9
Ferhad <mislim.sheridancollege.ca>
2023-03-19 00:14:50 -0400

    init backend branch

---
+++ src/main/java/ca/sheridancollege/blockheads/bridgingblock/BridgingBlockApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.blockheads.bridgingblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgingBlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BridgingBlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridancollege/blockheads/bridgingblock/BridgingBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.blockheads.bridgingblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgingBlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
