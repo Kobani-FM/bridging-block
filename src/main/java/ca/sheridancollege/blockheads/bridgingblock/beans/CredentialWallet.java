6f20a830c09b5e59f2e5e3a0a4177704c08bafef
Ferhad Mislim <mislim@sheridancollege.ca>
2023-04-08 21:17:50 -0400

    added CredentialWallet class

--- src/main/java/ca/sheridancollege/blockheads/bridgingblock/beans/CredentialWallet.java
+++ src/main/java/ca/sheridancollege/blockheads/bridgingblock/beans/CredentialWallet.java
@@ -1,2 +1,41 @@
-package ca.sheridancollege.blockheads.bridgingblock.beans;public class CredentialWallet {
+package ca.sheridancollege.blockheads.bridgingblock.beans;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Builder
+public class CredentialWallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonIgnore
+    @OneToMany(fetch=FetchType.LAZY, mappedBy = "credentialWallet", cascade = CascadeType.ALL)
+    private List<Certificate> certificates;
+/*
+    @JsonIgnore
+    @OneToOne
+    @JoinTable(name = "CREDENTIAL_WALLET_WALLET",
+            joinColumns = @JoinColumn(name = "CREDENTIAL_WALLET_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CREDENTIAL_WALLET_ID"))
+    private Wallet wallet;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinTable(name = "CREDENTIAL_WALLET_GRADUATE",
+            joinColumns = @JoinColumn(name = "CREDENTIAL_WALLET_ID"),
+            inverseJoinColumns = @JoinColumn(name = "GRADUATE_ID"))
+    private Graduate graduate;
+
+ */
 }
