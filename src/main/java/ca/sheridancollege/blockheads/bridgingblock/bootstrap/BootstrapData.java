10740569d76163bc5b13caaf20ec15f7b1391cbd
Timothy <quanti@sheridancollege.ca>
2023-04-11 04:29:32 -0400

    added some more samples to BootStrapData

--- src/main/java/ca/sheridancollege/blockheads/bridgingblock/bootstrap/BootstrapData.java
+++ src/main/java/ca/sheridancollege/blockheads/bridgingblock/bootstrap/BootstrapData.java
@@ -50,13 +50,41 @@ public class BootstrapData implements CommandLineRunner {
 				.institutions(emptyList).build();
 		g1.getInstitutions().add(intsRepo.findInstitutionByName("Sheridan College"));
 		g1 = gradRepo.save(g1);
-		
 		Wallet w1 = Wallet.builder().address(g1.getAccountAddress()).build();
 		w1 = walletRepo.save(w1);
-		
 		CredentialWallet cw1 = CredentialWallet.builder().accountAddress(g1.getAccountAddress())
 				.wallet(w1).graduate(g1).build();
 		cw1 = credRepo.save(cw1);
+		
+		
+		emptyList.clear();
+		Graduate g2 = Graduate.builder().firstName("Woof").lastName("Woof").email("woof@humbercollege.ca")
+				.StudentID(111111111).accountAddress("BbBbBbBbBbBbBb98765111BbBbBbBbBbBbBb4321dD")
+				.institutions(emptyList).build();
+		g2.getInstitutions().add(intsRepo.findInstitutionByName("Humber College"));
+		g2 = gradRepo.save(g2);
+		Wallet w2 = Wallet.builder().address(g2.getAccountAddress()).build();
+		w2 = walletRepo.save(w2);
+		CredentialWallet cw2 = CredentialWallet.builder().accountAddress(g2.getAccountAddress())
+				.wallet(w2).graduate(g2).build();
+		cw2 = credRepo.save(cw2);
+		
+		
+		emptyList.clear();
+		Graduate g3 = Graduate.builder().firstName("Bob").lastName("Builder").email("bobthebuilder@gmail.com")
+				.StudentID(555555555).accountAddress("02f23j0ogh2JHAFo2309hokh94u0t2gjokB9024hgi")
+				.institutions(emptyList).build();
+		g3.getInstitutions().add(intsRepo.findInstitutionByName("Sheridan College"));
+		g3.getInstitutions().add(intsRepo.findInstitutionByName("Seneca College"));
+		g3.getInstitutions().add(intsRepo.findInstitutionByName("George Brown College"));
+		g3 = gradRepo.save(g3);
+		
+		Wallet w3 = Wallet.builder().address(g3.getAccountAddress()).build();
+		w3 = walletRepo.save(w3);
+		
+		CredentialWallet cw3 = CredentialWallet.builder().accountAddress(g3.getAccountAddress())
+				.wallet(w3).graduate(g3).build();
+		cw3 = credRepo.save(cw3);
 
 	}
 
