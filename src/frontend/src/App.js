71a503ebcc9e47dd796f674e4501d9f19230dc65
Ferhad Mislim <mislim@sheridancollege.ca>
2023-10-24 18:40:11 -0400

    added & updated routes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import MetaMaskDetector from "./components/MetaMaskDetector";
-import Navigation from "./components/Navbar";
-import NavigationGraduate from "./components/NavbarGraduate";
-import NavigationInstitution from "./components/NavbarInstitution";
+import MetaMaskDetectorStudent from "./components/graduate/MetaMaskDetectorStudent";
+import MetaMaskConnectInstitution from "./components/instituation/MetaMaskConnectInstituation";
+import Navigation from "./components/navbar/Navbar";
+import NavigationGraduate from "./components/navbar/NavbarGraduate";
+import NavigationInstitution from "./components/navbar/NavbarInstitution";
 import Home from "./components/Home";
 import { BrowserRouter  } from 'react-router-dom';
 // import { Switch } from 'react-switch'
@@ -12,14 +13,15 @@ import Contact from "./components/Contact";
 
 import { Route, Switch, withRouter } from "react-router-dom";
 import React from "react";
-import Graduate from "./components/Graduate";
-import CredentialRequestForm from "./components/GraduateRequestCredentialForm";
-import GraduateViewCredentialRequest from "./components/GraduateViewCredentialRequest";
-import Institution from "./components/Institution";
-import InstitutionViewGraduate from "./components/InstitutionViewGraduate";
-import InstitutionViewRequest from "./components/InstitutionViewRequest";
-import InstitutionAddCredential from "./components/InstitutionAddCredential";
-import CredentialWallet from "./components/CredentialWallet";
+import Graduate from "./components/graduate/Graduate";
+import CredentialRequestForm from "./components/graduate/GraduateRequestCredentialForm";
+import GraduateViewCredentialRequest from "./components/graduate/GraduateViewCredentialRequest";
+import Institution from "./components/instituation/Institution";
+import InstitutionViewGraduate from "./components/instituation/InstitutionViewGraduate";
+import InstitutionViewRequest from "./components/instituation/InstitutionViewRequest";
+import InstitutionAddCredential from "./components/instituation/InstitutionAddCredential";
+import CredentialWallet from "./components/graduate/CredentialWallet";
+import MetaMaskDetectorInstituation from "./components/instituation/MetaMaskDetectorInstituation";
 
  const App = () =>
      (
@@ -34,13 +36,19 @@ import CredentialWallet from "./components/CredentialWallet";
                  <Route exact path="/create/credential-wallet" component={Graduate} />
                  <Route exact path="/graduate/create-credential-request" component={CredentialRequestForm} />
                  <Route exact path="/graduate/view-credential-requests" component={GraduateViewCredentialRequest} />
-                 <Route exact path="/institution" component={Institution} />
+
                  <Route exact path="/institution/view-graduates" component={InstitutionViewGraduate} />
-                 <Route exact path="/institution/view-credential-requests" component={InstitutionViewRequest} />
+
                  <Route exact path="/institution/add-credential" component={withRouter(InstitutionAddCredential)} />
                  {/*<Route exact path="/requester" component={Requester} />*/}
-                 <Route exact path="/create" component={MetaMaskDetector} />
+                 <Route exact path="/grad-meta-wallet" component={MetaMaskDetectorStudent} />
+                 <Route exact path="/ins-meta-wallet" component={MetaMaskConnectInstitution} />
                  <Route exact path="/access/credential-wallet" component={CredentialWallet} />
+                 {/*<Route exact path="/ins-meta-wallet/ins-register" component={}/>*/}
+                 <Route exact path="/ins-meta-wallet/ins-profile" component={Institution}/>
+                 <Route exact path="/ins-meta-wallet/view-credential-requests" component={InstitutionViewRequest} />
+                 {/*<Route exact path="/ins-meta-wallet/show-credential-request" component={}/>*/}
+
              </Switch>
          </React.StrictMode>
          // <div>
