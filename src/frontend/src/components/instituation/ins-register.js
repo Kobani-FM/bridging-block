8b09e83e08f1b66e68eb763eeb61eb8a498c219f
Ferhad Mislim <mislim@sheridancollege.ca>
2023-10-29 15:54:59 -0400

    requested to be an issuer in SC

--- src/frontend/src/components/instituation/ins-register.js
+++ src/frontend/src/components/instituation/ins-register.js
@@ -1,19 +1,33 @@
-// Not imp: need to add its fun to SC
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SmartContractInteraction from '../contract/SmartContractInteraction';
 import ContactABI from "../../constant/ContractABI.json";
-import {contractAddress} from '../../constant/config';
+import { contractAddress } from '../../constant/config';
+
 function RegisterInstitution() {
+    const [institutionName, setInstitutionName] = useState('');
+    const [userAddress, setUserAddress] = useState('');
 
-    const registerInstitution = async () => {
+    useEffect(() => {
+        if (typeof window.ethereum !== 'undefined') {
+            window.ethereum
+                .request({ method: 'eth_requestAccounts' })
+                .then((accounts) => {
+                    setUserAddress(accounts[0]);
+                })
+                .catch((error) => {
+                    console.error('Error connecting to MetaMask:', error);
+                });
+        }
+    }, []);
 
-        const contractInstance = SmartContractInteraction.getContractInstance(ContactABI,contractAddress);
+    const handleRegister = async () => {
+        const contractInstance = SmartContractInteraction.getContractInstance(ContactABI, contractAddress);
+
         try {
-            const tx = await contractInstance.methods.registerInstitution(
-                'Institution Address',
-                'Institution Name'
-            ).send({ from: 'Ethereum Address' });
+            const tx = await contractInstance.methods.issuerRequest(
+                userAddress,
+                institutionName
+            ).send({ from: userAddress });
 
             console.log('Transaction Hash:', tx.transactionHash);
         } catch (error) {
@@ -22,9 +36,39 @@ function RegisterInstitution() {
     };
 
     return (
-        <div>
-            <h2>Register an Institution</h2>
-            <button onClick={registerInstitution}>Register</button>
+        <div className="container mt-4">
+            <div className="card">
+                <div className="card-body">
+                    <h2 className="card-title">Request to be an issuer</h2>
+                    <form>
+                        <div className="form-group mb-3">
+                            <label htmlFor="institutionAddress">Your Ethereum Address:</label>
+                            <input
+                                type="text"
+                                id="institutionAddress"
+                                className="form-control"
+                                value={userAddress}
+                                disabled
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <label htmlFor="institutionName">Institution Name:</label>
+                            <input
+                                type="text"
+                                id="institutionName"
+                                className="form-control"
+                                value={institutionName}
+                                onChange={(e) => setInstitutionName(e.target.value)}
+                            />
+                        </div>
+                        <div className="text-center">
+                            <button type="button" className="btn btn-primary" onClick={handleRegister}>
+                                Register
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
